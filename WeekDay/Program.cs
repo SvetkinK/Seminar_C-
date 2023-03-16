6d17d1e75ad3c103cc4eb51bf908fce66a8e544e
Svetlana <khabarova09@rambler.ru>
2023-03-16 20:12:04 +0300

    День недели

---
+++ WeekDay/Program.cs
@@ -0,0 +1,14 @@
+﻿// На вход цифра обозначающая день недели, проверить выходной или нет
+
+Console.WriteLine("Введите день недели ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if(n>5 && n<8)
+{
+  Console.WriteLine("да");
+}
+if(n>0 && n<6)
+{
+  Console.WriteLine("нет");
+}
+
